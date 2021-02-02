e8f778a533256bfd126d5c3281433ff5cecac181
Cen3mode <msulej04@mail.com>
2021-02-03 00:12:14 +0100

    Demo is finally fully finished

--- sketch.js
+++ sketch.js
@@ -9,39 +9,49 @@ function setup() {
     tree = new Quadtree();
     particles = [];
 
-    particleCount = 200;
+    particleCount = 5000;
     for (var i = 0; i < particleCount; i++) {
         particles.push(new Point(random(1, width - 1), random(1, height - 1)));
     }
-
-    particles.forEach((particle, i) => {
-        console.log(tree.insert(particle));
-    });
-    console.log(tree);
-
 }
 
 function draw() {
+
+    tree = new Quadtree();
+    particles.forEach((particle, i) => {
+        tree.insert(particle);
+    });
+
     background(120);
     noFill();
-    stroke(255, 0, 0);
+    stroke(255);
+    strokeWeight(1);
+
+    let allParticles = tree.queryRange(new Area(0, 0, width, height));
+    let query = tree.queryRange(new Area(mouseX - 30, mouseY - 30, mouseX + 30, mouseY + 30));
+
     strokeWeight(2);
 
-    //console.log(particles);
-    let allPoints = tree.queryRange(new Area(0, 0, width, height));
-    let query = tree.queryRange(new Area(0, 0, 399, 399));
+    allParticles.forEach((particle, i) => {
+        point(particle.x, particle.y);
+    });
+
+    stroke(255, 0, 0);
 
     query.forEach((particle, i) => {
-        //console.log(particle)
-        strokeWeight(5);
         point(particle.x, particle.y);
-        strokeWeight(2);
     });
+
+    strokeWeight(1);
+
+    stroke(255);
+
+    rect(mouseX - 30, mouseY - 30, 60, 60);
 
     strokeWeight(0.5);
     stroke(100);
-    tree.show();
+    //tree.show();
 
-    //rect(mouseX - 30, mouseY - 30, 60, 60);
+
     return;
 }
